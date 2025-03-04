364fcdbdff90a2ffdb920c2b0923fe8b6b48a313
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-03-04 16:42:23 +0900

    :computer: bundle gem exprify

---
+++ lib/exprify.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "exprify/version"
+
+module Exprify
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/exprify/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Exprify
+  VERSION = "0.1.0"
+end
