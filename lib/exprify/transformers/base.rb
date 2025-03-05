4f82f2a6f4a260018d6eb3e8e7216633cec4682f
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-03-05 09:31:24 +0900

    :recycle: Move Base class to its own file
    - Created lib/exprify/transformers/base.rb
    - Moved Base class implementation from transformers.rb
    - Removed Base class from transformers.rb

---
+++ lib/exprify/transformers/base.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Exprify
+  module Transformers
+    class Base
+      def transform(ast)
+        ast.accept(self)
+      end
+
+      def visit_and(node)
+        raise NotImplementedError
+      end
+
+      def visit_or(node)
+        raise NotImplementedError
+      end
+
+      def visit_not(node)
+        raise NotImplementedError
+      end
+
+      def visit_keyword(node)
+        raise NotImplementedError
+      end
+
+      def visit_group(node)
+        raise NotImplementedError
+      end
+
+      def visit_exact_phrase(node)
+        raise NotImplementedError
+      end
+
+      def visit_named_argument(node)
+        raise NotImplementedError
+      end
+    end
+  end
+end
