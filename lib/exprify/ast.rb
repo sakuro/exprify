8257e1f6f6924dc747a1670925521c7604751827
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-03-05 10:32:53 +0900

    :police_officer: Remove documentation-related TODOs
    Remove Style/Documentation and Style/DocumentationMethod from .rubocop_todo.yml as they have been addressed

--- lib/exprify/ast.rb
+++ lib/exprify/ast.rb
@@ -178,21 +178,37 @@ module Exprify
 
     # Node representing a keyword
     class KeywordNode < Node
+      # @return [String] The keyword value
       attr_reader :value
 
+      # Initialize a new keyword node
+      #
+      # @param value [String] The keyword value
+      # @return [KeywordNode] A new keyword node
       def initialize(value)
         super()
         @value = value
       end
 
+      # Accept a visitor
+      #
+      # @param visitor [Object] The visitor object
+      # @return [Object] The result of the visit
       def accept(visitor)
         visitor.visit_keyword(self)
       end
 
+      # Return a string representation of the node
+      #
+      # @return [String] The string representation
       def inspect
         "#<KeywordNode value=#{value.inspect}>"
       end
 
+      # Pretty print the node
+      #
+      # @param pp [PP] The pretty printer
+      # @return [void]
       def pretty_print(pp)
         pp.text("KeywordNode")
         pp.nest(2) do
@@ -203,24 +219,43 @@ module Exprify
       end
     end
 
+    # Node representing a named argument with a name and value
     class NamedArgumentNode < Node
+      # @return [String] The argument name
       attr_reader :name
+      # @return [String] The argument value
       attr_reader :value
 
+      # Initialize a new named argument node
+      #
+      # @param name [String] The argument name
+      # @param value [String] The argument value
+      # @return [NamedArgumentNode] A new named argument node
       def initialize(name, value)
         super()
         @name = name
         @value = value
       end
 
+      # Accept a visitor
+      #
+      # @param visitor [Object] The visitor object
+      # @return [Object] The result of the visit
       def accept(visitor)
         visitor.visit_named_argument(self)
       end
 
+      # Return a string representation of the node
+      #
+      # @return [String] The string representation
       def inspect
         "#<NamedArgumentNode name=#{name.inspect} value=#{value.inspect}>"
       end
 
+      # Pretty print the node
+      #
+      # @param pp [PP] The pretty printer
+      # @return [void]
       def pretty_print(pp)
         pp.text("NamedArgumentNode")
         pp.nest(2) do
@@ -234,22 +269,39 @@ module Exprify
       end
     end
 
+    # Node representing a group (parenthesized expression)
     class GroupNode < Node
+      # @return [Node] The grouped expression
       attr_reader :expression
 
+      # Initialize a new group node
+      #
+      # @param expression [Node] The grouped expression
+      # @return [GroupNode] A new group node
       def initialize(expression)
         super()
         @expression = expression
       end
 
+      # Accept a visitor
+      #
+      # @param visitor [Object] The visitor object
+      # @return [Object] The result of the visit
       def accept(visitor)
         visitor.visit_group(self)
       end
 
+      # Return a string representation of the node
+      #
+      # @return [String] The string representation
       def inspect
         "#<GroupNode expression=#{expression.inspect}>"
       end
 
+      # Pretty print the node
+      #
+      # @param pp [PP] The pretty printer
+      # @return [void]
       def pretty_print(pp)
         pp.text("GroupNode")
         pp.nest(2) do
@@ -260,22 +312,39 @@ module Exprify
       end
     end
 
+    # Node representing an exact phrase to match
     class ExactPhraseNode < Node
+      # @return [String] The exact phrase
       attr_reader :phrase
 
+      # Initialize a new exact phrase node
+      #
+      # @param phrase [String] The exact phrase
+      # @return [ExactPhraseNode] A new exact phrase node
       def initialize(phrase)
         super()
         @phrase = phrase
       end
 
+      # Accept a visitor
+      #
+      # @param visitor [Object] The visitor object
+      # @return [Object] The result of the visit
       def accept(visitor)
         visitor.visit_exact_phrase(self)
       end
 
+      # Return a string representation of the node
+      #
+      # @return [String] The string representation
       def inspect
         "#<ExactPhraseNode phrase=#{phrase.inspect}>"
       end
 
+      # Pretty print the node
+      #
+      # @param pp [PP] The pretty printer
+      # @return [void]
       def pretty_print(pp)
         pp.text("ExactPhraseNode")
         pp.nest(2) do
